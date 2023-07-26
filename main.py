46a8aabce3bb113ac719148f0357d91a935cd994
Jonathan Lever <121318635+JonthLever@users.noreply.github.com>
2023-07-26 14:22:14 -0600

    delete main.py

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import random
-import xlrd
-import xlsxwriter
-import os
 
-filePath="C:\\Users\\mecaj\\Documents\\Future\\PYTHON\\consulta.xls"
-openFile=xlrd.open_workbook(filePath);
-sheet=openFile.sheet_by_name("InformacionDTE-FEL")
-workbook = xlsxwriter.Workbook('Nuevo.xls')
-worksheet = workbook.add_worksheet()
